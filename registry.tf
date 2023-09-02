7e7e91855546c7823ea0254f77cce14691336ed7
manoj <ankireddy828@gmail.com>
2023-09-02 10:36:18 +0530

    login

--- registry.tf
+++ registry.tf
@@ -1,12 +1,6 @@
 
-data "docker_registry" "example" {
-  server = "https://index.docker.io/v1/"
-}
-
-# Docker login resource
-resource "docker_login" "example" {
-  name     = "myregistry.example.com"
-  registry = data.docker_registry.example.id
-  email    = "kishoreaj1995@gmail.com"
-  password = "Kishore012"
+resource "null_resource" "docker_login" {
+  provisioner "local-exec" {
+    command = "docker login -u kishoreaj1995@gmail.com -p Kishore012"
+  }
 }
