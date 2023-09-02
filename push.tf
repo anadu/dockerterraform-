1e3ce1bd612d79c2fa5044a642cf03bb95f86765
manoj <ankireddy828@gmail.com>
2023-09-02 10:54:20 +0530

    super

--- push.tf
+++
@@ -1,4 +0,0 @@
-resource "docker_registry_image" "zoo" {
-  name          = "000100000/terraform:latest" # Replace with your registry information
-  image_id      = docker_image.terraform.image_id
-}
