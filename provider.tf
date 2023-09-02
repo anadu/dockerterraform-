18362a78c4093f0260c1952072cd5016b67473d9
manoj <ankireddy828@gmail.com>
2023-09-02 10:34:09 +0530

    super

--- provider.tf
+++ provider.tf
@@ -11,14 +11,3 @@ provider "docker" {
   host        = "unix:///var/run/docker.sock"
 }
 
-data "docker_registry" "example" {
-  server = "https://index.docker.io/v1/"
-}
-
-# Docker login resource
-resource "docker_login" "example" {
-  name     = "myregistry.example.com"
-  registry = data.docker_registry.example.id
-  email    = "kishoreaj1995@gmail.com"
-  password = "Kishore012"
-}
--- registry.tf
+++ registry.tf
@@ -1,4 +1,12 @@
-resource "docker_registry_image" "helloworld" {
-  name          = terraform2
-  keep_remotely = true
+
+data "docker_registry" "example" {
+  server = "https://index.docker.io/v1/"
+}
+
+# Docker login resource
+resource "docker_login" "example" {
+  name     = "myregistry.example.com"
+  registry = data.docker_registry.example.id
+  email    = "kishoreaj1995@gmail.com"
+  password = "Kishore012"
 }
