bd365c6c6292d9eda77d9ccc01d535083a16b1a2
manoj <ankireddy828@gmail.com>
2023-09-02 10:19:07 +0530

    super

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "docker" {
   host        = "unix:///var/run/docker.sock"
-  config_path = "/root/.docker/config.json."
+  config_path = "/root/.docker/config.json"
   email       = "kishoreaj1995@gmail.com"
   password    = "kishore012"
 
