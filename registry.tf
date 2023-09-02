eb1986eb26bd4f676929091377c11df23c415317
manoj <ankireddy828@gmail.com>
2023-09-02 12:22:59 +0530

    super

--- registry.tf
+++ registry.tf
@@ -1,5 +1,6 @@
 
-provisioner "local-exec" {
+resource "null_resource" "docker_login" {
+  provisioner "local-exec" {
   command = <<EOT
     echo "${var.password}" | docker login -u "${var.username}" --password-stdin
   EOT
